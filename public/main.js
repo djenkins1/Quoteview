00f575ec9bb8d6938321163f90c9c8c70c2ffda2
Dilan Jenkins <dilan@Dilans-Mac-mini.lan>
2017-11-08 19:07:01 -0700

    Quotes are now resorted client side when voted on

--- public/main.js
+++ public/main.js
@@ -1,3 +1,108 @@
+//object containing all the quotes by their id being the key
+var allQuotes = {};
+
+//array that contains ids of quotes that are in descending order by their score
+var quotesByScore = [];
+
+//swap the quote that has been updated if it needs to be swapped
+//return true if the list was modified or false otherwise
+function updateQuoteByScore( quoteDict, list, quoteId )
+{
+    //find the quote and swap it if needed
+    var i;
+    for( i = 0; i < list.length; i++ )
+    {
+        if ( list[ i ] == quoteId )
+        {
+            break;
+        }
+    }
+
+    //if the whole list was traversed without finding the quote then log to console and return
+    if ( i >= list.length )
+    {
+        console.log( "Quote with id not in list yet:" , quoteId );
+        return false;
+    }
+
+    var myQuote = quoteDict[ list[ i ] ];
+    
+    //check to see if quote has score higher than quote to the left
+    //  if so then swap and return true
+    var leftQuote = list[ i - 1 ];
+    if ( leftQuote !== undefined && quoteDict[ leftQuote ].score < myQuote.score )
+    {
+        list[ i - 1] = myQuote.qid;
+        list[ i ] = leftQuote;
+        return true;
+    }
+    
+    //otherwise check to see if quote has lower score than quote to the right
+    //  if so then swap and return true
+    var rightQuote = list[ i + 1 ];
+    if ( rightQuote !== undefined && quoteDict[ rightQuote ].score > myQuote.score )
+    {
+        list[ i + 1] = myQuote.qid;
+        list[ i ] = rightQuote;
+        return true;
+    }
+
+    //otherwise, no need to swap,return false
+    return false;
+}
+
+//inserts the quote with id given into the list given in a position so that the list is sorted
+//assumes that the list is already sorted by score before adding the new quote
+function insertQuoteByScore( quoteDict, list, quoteId )
+{
+    //if the list is empty then the new quote must go at the beginning of the empty list,so push it and return
+    if ( list.length == 0 )
+    {
+        list.push( quoteId );
+        return;
+    }
+
+    var myQuote = quoteDict[ quoteId ];
+    var i;
+    //starting at the beginning of the list,look for the first element with score less than the quote to be inserted
+    //once that element is found,use the value of i as where to insert the new quote
+    //if the element is not found,the new quote goes at the end
+    for (i = 0; i < list.length; i++ )
+    {
+        var atQuote = quoteDict[ list[ i ] ];
+        if ( atQuote.score < myQuote.score )
+        {
+            break;
+        }
+    }
+
+    //if the whole list was traversed and there were no elements with a smaller score than the new quote
+    //  then the new quote goes at the end of the list,so just push it and return
+    if ( i >= list.length )
+    {
+        list.push( quoteId );
+        return;
+    }
+
+    //insert the new quote at position i
+    list.splice( i , 0 , quoteId );
+}
+
+function updateQuotesView( quoteDict, list )
+{
+    var quoteDivs = $( "#quoteList" ).children();
+    for ( var i = 0; i < list.length; i++ )
+    {
+        var myQuote = quoteDict[ list[ i ] ];
+        //update the author,body and score
+        //also update the data qid for both left,score and right buttons
+        quoteDivs.eq( i ).find( ".quoteScore" ).text( myQuote.score );
+        quoteDivs.eq( i ).find( ".quoteBadge" ).data( "qid" , myQuote.qid );
+        quoteDivs.eq( i ).find( ".quoteBody" ).text( myQuote.body );
+        quoteDivs.eq( i ).find( ".quoteAuthor" ).text( "--" + myQuote.author );
+    }
+}
+
 //this function handles when the upvote/downvote button have been clicked on a specific quote
 //it sends a post request to the server and then updates the score for that quote when the server response gets back
 function handleVoteClick( e )
@@ -10,7 +115,9 @@ function handleVoteClick( e )
             return;
         }
 
-        $( "#quoteScore" + data.qid ).text( data.score );
+        allQuotes[ data.qid ] = data;
+        updateQuoteByScore( allQuotes, quotesByScore, data.qid );
+        updateQuotesView( allQuotes, quotesByScore );     
     });
 
     e.preventDefault();
@@ -22,7 +129,6 @@ function handleVoteClick( e )
 function createAndAppendQuoteDiv( myQuote )
 {
     var newItem = $( "<div />" );
-    newItem.attr( "id" , "quoteDiv" + myQuote.qid );
     newItem.addClass( "list-group-item" );
     newItem.addClass( "d-flex" );
     newItem.addClass( "flex-column" );
@@ -31,13 +137,15 @@ function createAndAppendQuoteDiv( myQuote )
 
     var newItemQuoteDiv = $( "<div />" );
     var newItemQuote = $( "<q />" );
+    newItemQuote.addClass( "quoteBody" );
     newItemQuote.text( myQuote.body );
 
     var newItemAuthor = $( "<div />" );
     newItemAuthor.text( "--" + myQuote.author );
+    newItemAuthor.addClass( "quoteAuthor" );
 
     var newBadgeContainer = $( "<div />" );
-    newBadgeContainer.attr( "id" , "badgeContain" );
+    newBadgeContainer.addClass( "badgeContain" );
     newBadgeContainer.addClass( "align-items-end" );
     newBadgeContainer.addClass( "align-self-end" );
     newBadgeContainer.css( "position" , "relative" );
@@ -54,11 +162,11 @@ function createAndAppendQuoteDiv( myQuote )
     newUpvoteBadge.click( handleVoteClick );
 
     var newScoreBadge = $( "<span />" );
-    newScoreBadge.attr( "id" , "quoteScore" + myQuote.qid );
     newScoreBadge.addClass( "badge" );
     newScoreBadge.addClass( "badge-primary" );
     newScoreBadge.addClass( "badge-pill" );
     newScoreBadge.addClass( "quoteBadge" );
+    newScoreBadge.addClass( "quoteScore" );
     newScoreBadge.text( myQuote.score );
 
     var newDownvoteBadge = $( "<a />" );
@@ -84,15 +192,16 @@ function createAndAppendQuoteDiv( myQuote )
 //this function handles when the server sends a response back for a new quote that was added
 function handleAddQuote( data, status )
 {
-    console.log( "Add Quote Data: " , data );
     if ( data.qid == undefined )
     {
         console.log( "SERVER DID NOT CREATE QUOTE" )
         return;
     }
-    //TODO: put the quote in the correct place based on its score
 
+    allQuotes[ data.qid ] = data;
+    insertQuoteByScore( allQuotes, quotesByScore, data.qid );
     createAndAppendQuoteDiv( data );
+    updateQuotesView( allQuotes, quotesByScore );
 }
 
 function handleNewQuoteForm( e )
@@ -116,12 +225,13 @@ function()
 {
     //TODO: rate limit upvote/downvote of quotes so that can only vote once per second
     //TODO: user login/password and creator field for quotes
-    //TODO: redo listing of quotes when a quote has been upvoted/downvoted(do this client side)
 
     $.get( "/quotes" , function( data, status )
     {
         for ( var i = 0; i < data.length; i++ )
         {
+            allQuotes[ data[ i ].qid ] = data[ i ];
+            insertQuoteByScore( allQuotes, quotesByScore, data[ i ].qid );
             createAndAppendQuoteDiv( data[ i ] );
         }
     });
@@ -141,12 +251,4 @@ function()
         $( "#basicModal" ).modal();
         e.preventDefault();
     });
-
-    /*
-    var dateObj = new Date();
-    $.post( "/newQuote" , { "author" : "CPU" , "body" : dateObj.getTime() }, function( data, status )
-    {
-        console.log( data );
-    });
-    */
 });
